138a2761b871f11d52c2595a4e4ffa22db262be5
Alex Ciminian <alex.ciminian@gmail.com>
2017-07-25 12:04:37 +0100

    Add tiled mandelbrot generator and static (CBR) tapestry

--- src/index.js
+++ src/index.js
@@ -2,41 +2,97 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import {createCells, restart, tick} from './color-automata';
 
-import Tapestry from './wasm/tapestry2.js';
+import TapestryModule from './wasm/tapestry.js';
+
+import MandelbrotModule from './wasm/mandelbrot.js'
 
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
 const wasmHelloWorld = () => {
-  Tapestry({wasmBinaryFile: 'wasm/tapestry2.wasm'}).then(tapestry => {
-    tapestry.addOnPostRun(() => {
+  TapestryModule({wasmBinaryFile: 'wasm/tapestry.wasm'}).then(Tapestry => {
+    Tapestry.addOnPostRun(() => {
       const canvas = document.getElementById('displayCanvas');
+      const context = canvas.getContext('2d');
 
       // Canvas resizing from http://stackoverflow.com/a/43364730/2142626
-      const width = canvas.clientWidth;
-      const height = canvas.clientHeight;
+      const width = window.innerWidth;
+      const height = window.innerHeight;
       if (canvas.width !== width || canvas.height !== height) {
           canvas.width = width;
           canvas.height = height;
       }
       console.log(width, height);
       console.time('mandelbrot');
-      const mandelbrot = tapestry.mandelbrot(width, height, 1, -0.5, 0);
+      const tapestry = new Tapestry.Tapestry(width, height);
       console.timeEnd('mandelbrot');
 
-      console.time('canvas put image data');
-      const imageData = new ImageData(new Uint8ClampedArray(mandelbrot), width, height);
+      function drawTile() {
+        console.time('compute tile');
+        const tile = tapestry.nextTile();
+        console.timeEnd('compute tile');
+        if (tile) {
+          console.time('import image data');
+          const imageData = new ImageData(new Uint8ClampedArray(tile.data), tile.width, tile.height);
+          console.timeEnd('import image data');
+          console.time('put image data');
+          context.putImageData(imageData, tile.x, tile.y);
+          console.timeEnd('put image data');
+          window.requestAnimationFrame(drawTile);
+        }
+      }
+      window.requestAnimationFrame(drawTile);
+
+
+      // console.time('canvas create image data');
+      // const imageData = new ImageData(new Uint8ClampedArray(mandelbrot), width, height);
+      // console.timeEnd('canvas put image data');
+      // console.time('canvas put image data');
       // const context = canvas.getContext('2d');
       // context.putImageData(imageData, 0, 0);
-      console.timeEnd('canvas put image data');
+      // console.timeEnd('canvas put image data');
     }, err => { throw err; });
   });
 }
 
 window.onload = wasmHelloWorld;
 
-window.addEventListener("load", canvasApp, false);
+
+// const mandelbrotTiling = () => {
+//   Mandelbrot({wasmBinaryFile: 'wasm/mandelbrot.wasm'}).then(Mandelbrot => {
+//     Mandelbrot.addOnPostRun(() => {
+//       const canvas = document.getElementById('displayCanvas');
+//       const context = canvas.getContext('2d');
+
+//       // Canvas resizing from http://stackoverflow.com/a/43364730/2142626
+//       const width = window.innerWidth;
+//       const height = window.innerHeight;
+//       if (canvas.width !== width || canvas.height !== height) {
+//           canvas.width = width;
+//           canvas.height = height;
+//       }
+
+//       const mandelbrot = new Mandelbrot.Mandelbrot(width, height, 1, -0.5, 0);
+
+//       function drawTile() {
+//         const tile = mandelbrot.nextTile();
+//         if (tile) {
+//             console.time('image data');
+//             const imageData = new ImageData(new Uint8ClampedArray(tile.data), tile.width, tile.height);
+//             console.timeEnd('image data');
+//             context.putImageData(imageData, tile.x, tile.y);
+//             window.requestAnimationFrame(drawTile);
+//         }
+//       }
+//       window.requestAnimationFrame(drawTile);
+//     });
+//   });
+// }
+
+// window.onload = mandelbrotTiling;
+
+// window.addEventListener("load", canvasApp, false);
 
 function canvasApp() {
   const displayCanvas = document.getElementById("displayCanvas");
--- src/wasm/tapestry.cpp
+++ src/wasm/tapestry.cpp
@@ -1,168 +1,90 @@
-/*
- * Copyright 2017 Google Inc. All rights reserved.
- *
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
- * file except in compliance with the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
- * ANY KIND, either express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
-#include <emscripten/bind.h>
 #include <cstddef>
 #include <cstdlib>
+#include <ctime>
+#include <emscripten/bind.h>
 
 // CSV to RGB conversion from http://stackoverflow.com/questions/3018313/
-typedef struct
-{
+typedef struct {
   double r; // a fraction between 0 and 1
   double g; // a fraction between 0 and 1
   double b; // a fraction between 0 and 1
 } rgb;
 
-typedef struct
-{
+typedef struct {
   double h; // angle in degrees
   double s; // a fraction between 0 and 1
   double v; // a fraction between 0 and 1
 } hsv;
 
-static rgb hsv2rgb(hsv in);
+typedef struct {
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
+} pixel;
 
-rgb hsv2rgb(hsv in)
-{
-  double hh, p, q, t, ff;
-  long i;
-  rgb out;
+const int TILE_SIZE = 1024;
 
-  if (in.s <= 0.0)
-  { // < is bogus, just shuts up warnings
-    out.r = in.v;
-    out.g = in.v;
-    out.b = in.v;
-    return out;
-  }
-  hh = in.h;
-  if (hh >= 360.0)
-    hh = 0.0;
-  hh /= 60.0;
-  i = (long)hh;
-  ff = hh - i;
-  p = in.v * (1.0 - in.s);
-  q = in.v * (1.0 - (in.s * ff));
-  t = in.v * (1.0 - (in.s * (1.0 - ff)));
+class Tapestry {
+private:
+  int width;
+  int height;
 
-  switch (i)
-  {
-  case 0:
-    out.r = in.v;
-    out.g = t;
-    out.b = p;
-    break;
-  case 1:
-    out.r = q;
-    out.g = in.v;
-    out.b = p;
-    break;
-  case 2:
-    out.r = p;
-    out.g = in.v;
-    out.b = t;
-    break;
+  // generate tapestry in tiles
+  int currentTileX = 0;
+  int currentTileY = 0;
 
-  case 3:
-    out.r = p;
-    out.g = q;
-    out.b = in.v;
-    break;
-  case 4:
-    out.r = t;
-    out.g = p;
-    out.b = in.v;
-    break;
-  case 5:
-  default:
-    out.r = in.v;
-    out.g = p;
-    out.b = q;
-    break;
-  }
-  return out;
-}
+  // The image buffer handed back to JS for rendering into canvas
+  // The image format that imageData expects is four unsigned bytes: red,
+  // green, blue, alpha
+  uint8_t buffer[TILE_SIZE * TILE_SIZE * 4];
 
-uint8_t *buffer = nullptr;
-size_t bufferSize = 0;
+public:
+  Tapestry(int width, int height)
+    : width(width), height(height) {}
 
-// Mandlebrot definition from http://lodev.org/cgtutor/juliamandelbrot.html
-
-emscripten::val mandelbrot(int w, int h, double zoom, double moveX, double moveY)
-{
-  if (buffer != nullptr)
-  {
-    free(buffer);
-  }
-
-  // The image format that imageData expects is four unsigned bytes: red, green, blue, alpha
-  bufferSize = w * h * 4;
-  buffer = (uint8_t *)malloc(bufferSize);
-  if (buffer == nullptr)
-  {
-    // Following the JavaScript idiom that undefined is error
-    return emscripten::val::undefined();
-  }
-
-  // each iteration, it calculates: newz = oldz*oldz + p, where p is the current pixel, and oldz stars at the origin
-  double pr, pi;                     // real and imaginary part of the pixel p
-  double newRe, newIm, oldRe, oldIm; // real and imaginary parts of new and old z
-  rgb color;                         // the RGB color value for the pixel
-  int maxIterations = 360;           // after how much iterations the function should stop. Chosen to make take up full HSV hue range.
-
-  // loop through every pixel
-  for (int y = 0; y < h; y++)
-    for (int x = 0; x < w; x++)
-    {
-      // calculate the initial real and imaginary part of z, based on the pixel location and zoom and position values
-      pr = 1.5 * (x - w / 2) / (0.5 * zoom * w) + moveX;
-      pi = (y - h / 2) / (0.5 * zoom * h) + moveY;
-      newRe = newIm = oldRe = oldIm = 0; //these should start at 0,0
-      // "i" will represent the number of iterations
-      int i;
-      // start the iteration process
-      for (i = 0; i < maxIterations; i++)
-      {
-        // remember value of previous iteration
-        oldRe = newRe;
-        oldIm = newIm;
-        // the actual iteration, the real and imaginary part are calculated
-        newRe = oldRe * oldRe - oldIm * oldIm + pr;
-        newIm = 2 * oldRe * oldIm + pi;
-        // if the point is outside the circle with radius 2: stop
-        if ((newRe * newRe + newIm * newIm) > 4)
-          break;
-      }
-      // use color model conversion to get rainbow palette, make brightness black if maxIterations reached
-      hsv hsvColor;
-      hsvColor.h = i;
-      hsvColor.s = 1; // fully saturated.
-      hsvColor.v = i < maxIterations;
-      color = hsv2rgb(hsvColor);
-      //draw the pixel
-      size_t bufferOffset = (x + y * w) * 4;
-      buffer[bufferOffset + 0] = color.r * 255;
-      buffer[bufferOffset + 1] = color.g * 255;
-      buffer[bufferOffset + 2] = color.b * 255;
-      buffer[bufferOffset + 3] = 255;
+  emscripten::val nextTile() {
+    if (this->currentTileY * TILE_SIZE > this->height) {
+      this->currentTileY = 0;
     }
 
-  return emscripten::val(emscripten::typed_memory_view(bufferSize, buffer));
-}
+    // Generate a TILE_SIZE x TILE_SIZE array of pixels
+    for (int y = this->currentTileY * TILE_SIZE;
+         y < (this->currentTileY + 1) * TILE_SIZE; y++) {
+      for (int x = this->currentTileX * TILE_SIZE;
+           x < (this->currentTileX + 1) * TILE_SIZE; x++) {
+        // draw the pixel
+        size_t bufferOffset =
+            ((x - this->currentTileX * TILE_SIZE) +
+             (y - this->currentTileY * TILE_SIZE) * TILE_SIZE) *
+            4;
+        this->buffer[bufferOffset + 0] = std::rand() % 255 + 1;
+        this->buffer[bufferOffset + 1] = std::rand() % 255 + 1;
+        this->buffer[bufferOffset + 2] = std::rand() % 255 + 1;
+        this->buffer[bufferOffset + 3] = 255;
+      }
+    }
 
-EMSCRIPTEN_BINDINGS(hello)
-{
-  emscripten::function("mandelbrot", &mandelbrot);
+    emscripten::val returnVal = emscripten::val::object();
+    returnVal.set("data", emscripten::val(emscripten::typed_memory_view(
+                              TILE_SIZE * TILE_SIZE * 4, this->buffer)));
+    returnVal.set("width", emscripten::val(TILE_SIZE));
+    returnVal.set("height", emscripten::val(TILE_SIZE));
+    returnVal.set("x", emscripten::val(this->currentTileX * TILE_SIZE));
+    returnVal.set("y", emscripten::val(this->currentTileY * TILE_SIZE));
+
+    // Increment to the next tile
+    this->currentTileX++;
+    if (this->currentTileX * TILE_SIZE > this->width) {
+      this->currentTileX = 0;
+      this->currentTileY++;
+    }
+
+    return returnVal;
+  }
+};
+
+EMSCRIPTEN_BINDINGS(hello) {
+  emscripten::class_<Tapestry>("Tapestry")
+      .constructor<int, int>()
+      .function("nextTile", &Tapestry::nextTile);
 }
