43c6e6c09440f63886510d3d31d3a7283fb8af4a
Alex Ciminian <alex.ciminian@gmail.com>
2017-07-26 01:03:52 +0100

    Implement a correct color automata running in wasm

--- src/index.js
+++ src/index.js
@@ -10,89 +10,42 @@ import TapestryModule from './wasm/tapestry.js';
 // ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
-const wasmHelloWorld = () => {
+const wasmRenderTapestry = () => {
   TapestryModule({wasmBinaryFile: 'wasm/tapestry.wasm'}).then(Tapestry => {
     Tapestry.addOnPostRun(() => {
       const canvas = document.getElementById('displayCanvas');
       const context = canvas.getContext('2d');
 
-      // Canvas resizing from http://stackoverflow.com/a/43364730/2142626
-      const width = window.innerWidth;
-      const height = window.innerHeight;
+      const width = window.innerWidth / 2;
+      const height = window.innerHeight / 2;
       if (canvas.width !== width || canvas.height !== height) {
           canvas.width = width;
           canvas.height = height;
       }
-      console.log(width, height);
       console.time('initialization');
       const tapestry = new Tapestry.Tapestry(width, height);
       console.timeEnd('initialization');
 
-      function drawTile() {
-        console.time('compute tile');
-        const tile = tapestry.nextTile();
-        console.timeEnd('compute tile');
-        if (tile) {
-          // console.time('import image data');
-          const imageData = new ImageData(new Uint8ClampedArray(tile.data), tile.width, tile.height);
-          // console.timeEnd('import image data');
-          // console.time('put image data');
-          context.putImageData(imageData, tile.x, tile.y);
-          // console.timeEnd('put image data');
-          window.requestAnimationFrame(drawTile);
-        }
-      }
-      window.requestAnimationFrame(drawTile);
+      setInterval(() => {
+        console.time('wasm extract image');
+        const tile = tapestry.fullImage();
+        console.timeEnd('wasm extract image');
 
+        console.time('canvas put image data');
+        const imageData = new ImageData(new Uint8ClampedArray(tile), width, height);
+        context.putImageData(imageData, 0, 0);
+        console.timeEnd('canvas put image data');
 
-      // console.time('canvas create image data');
-      // const imageData = new ImageData(new Uint8ClampedArray(mandelbrot), width, height);
-      // console.timeEnd('canvas put image data');
-      // console.time('canvas put image data');
-      // const context = canvas.getContext('2d');
-      // context.putImageData(imageData, 0, 0);
-      // console.timeEnd('canvas put image data');
-    }, err => { throw err; });
+        console.time('wasm compute image');
+        tapestry.tick();
+        console.timeEnd('wasm compute image');
+
+      }, 1000 / 30);
+    });
   });
 }
 
-window.onload = wasmHelloWorld;
-
-
-// const mandelbrotTiling = () => {
-//   Mandelbrot({wasmBinaryFile: 'wasm/mandelbrot.wasm'}).then(Mandelbrot => {
-//     Mandelbrot.addOnPostRun(() => {
-//       const canvas = document.getElementById('displayCanvas');
-//       const context = canvas.getContext('2d');
-
-//       // Canvas resizing from http://stackoverflow.com/a/43364730/2142626
-//       const width = window.innerWidth;
-//       const height = window.innerHeight;
-//       if (canvas.width !== width || canvas.height !== height) {
-//           canvas.width = width;
-//           canvas.height = height;
-//       }
-
-//       const mandelbrot = new Mandelbrot.Mandelbrot(width, height, 1, -0.5, 0);
-
-//       function drawTile() {
-//         const tile = mandelbrot.nextTile();
-//         if (tile) {
-//             console.time('image data');
-//             const imageData = new ImageData(new Uint8ClampedArray(tile.data), tile.width, tile.height);
-//             console.timeEnd('image data');
-//             context.putImageData(imageData, tile.x, tile.y);
-//             window.requestAnimationFrame(drawTile);
-//         }
-//       }
-//       window.requestAnimationFrame(drawTile);
-//     });
-//   });
-// }
-
-// window.onload = mandelbrotTiling;
-
-// window.addEventListener("load", canvasApp, false);
+window.onload = wasmRenderTapestry;
 
 function canvasApp() {
   const displayCanvas = document.getElementById("displayCanvas");
@@ -102,13 +55,14 @@ function canvasApp() {
   const displayWidth = displayCanvas.width;
   const displayHeight = displayCanvas.height;
 
-  const gridWidth = 120;
-  const gridHeight = 72;
+  const gridWidth = 512;
+  const gridHeight = 128;
 
   const cellSize = {
     width: displayWidth / gridWidth,
     height: displayHeight / gridHeight
   };
+
   const options = {
     ease: 0.67,
     velMax: 255,
--- src/wasm/tapestry.cpp
+++ src/wasm/tapestry.cpp
@@ -2,12 +2,18 @@
 #include <cstdlib>
 #include <ctime>
 #include <emscripten/bind.h>
+#include <math.h>
 
 typedef struct {
-  uint8_t r;
-  uint8_t g;
-  uint8_t b;
+  double r;
+  double g;
+  double b;
 } pixel;
+
+typedef struct {
+  int x;
+  int y;
+} pos;
 
 const int TILE_SIZE = 1024;
 
@@ -30,8 +36,15 @@ private:
   pixel *velocityPrimary = nullptr;
   pixel *velocityBuffer = nullptr;
 
+  // we've learned vectors now
+  std::vector<std::vector<pos>> neighbors;
+
+  pixel *initBuffer() {
+    return (pixel *)malloc(this->width * this->height * sizeof(pixel));
+  }
+
   pixel *initBufferToRandomColors() {
-    pixel *output = (pixel *)malloc(this->width * this->height);
+    pixel *output = this->initBuffer();
     for (int y = 0; y < this->height; y++) {
       for (int x = 0; x < this->width; x++) {
         int addr = y * this->width + x;
@@ -43,30 +56,113 @@ private:
     return output;
   }
 
-  pixel *copyBufferValues(pixel *input) {
-    pixel *output = (pixel *)malloc(this->width * this->height);
-    for (int y = 0; y < this->height; y++) {
-      for (int x = 0; x < this->width; x++) {
-        size_t addr = y * this->width + x;
-        output[addr].r = input[addr].r;
-        output[addr].g = input[addr].g;
-        output[addr].b = input[addr].b;
-      }
-    }
-    return output;
+  void copyBufferValues(pixel *input, pixel *output) {
+    memcpy(output, input, this->width * this->height * sizeof(pixel));
   }
 
   pixel *initBufferToZero() {
-    pixel *output = (pixel *)malloc(this->width * this->height);
+    pixel *output = this->initBuffer();
+    memset(output, 0, this->height * this->width);
+    return output;
+  }
+
+  std::vector<pos> computeNeighbors(int x, int y) {
+    std::vector<pos> neighbors;
+    if (y > 0) {
+      pos coords = { .x = x, .y = y - 1 };
+      neighbors.push_back(coords);
+    }
+    if (x > 0) {
+      pos coords = { .x = x - 1, .y = y };
+      neighbors.push_back(coords);
+    }
+    if (y < this->height - 1) {
+      pos coords = {.x = x, .y = y + 1};
+      neighbors.push_back(coords);
+    }
+    if (x < this->width - 1) {
+      pos coords = {.x = x + 1, .y = y };
+      neighbors.push_back(coords);
+    }
+    return neighbors;
+  }
+
+  void initNeighbors() {
+    this->neighbors.resize(this->width * this->height);
     for (int y = 0; y < this->height; y++) {
       for (int x = 0; x < this->width; x++) {
-        int addr = y * this->width + x;
-        output[addr].r = 0;
-        output[addr].g = 0;
-        output[addr].b = 0;
+        this->neighbors[y * this->width + x] = this->computeNeighbors(x, y);
       }
     }
-    return output;
+  }
+
+  void add(pixel *a, pixel *b, pixel *result) {
+    result->r = a->r + b->r;
+    result->g = a->g + b->g;
+    result->b = a->b + b->b;
+  }
+
+  void scale(pixel *a, double value, pixel *result) {
+    result->r = a->r * value;
+    result->g = a->g * value;
+    result->b = a->b * value;
+  }
+
+  void subtract(pixel *a, pixel *b, pixel *result) {
+    result->r = a->r - b->r;
+    result->g = a->g - b->g;
+    result->b = a->b - b->b;
+  }
+
+  double sumOfSquares(pixel* p) {
+    return p->r * p->r + p->g * p->g + p->b * p->b;
+  }
+
+  int getOffset(int x, int y) {
+    return x + y * this->width;
+  }
+
+  void fixBoundaries(int cellOffset) {
+    // bounce colors off of color cube boundaries
+    if (this->colorBuffer[cellOffset].r < 0) {
+      this->colorBuffer[cellOffset].r = 0;
+      this->velocityBuffer[cellOffset].r *= -1;
+    } else if (this->colorBuffer[cellOffset].r > 255) {
+      this->colorBuffer[cellOffset].r = 255;
+      this->velocityBuffer[cellOffset].r *= -1;
+    }
+
+    if (this->colorBuffer[cellOffset].g < 0) {
+      this->colorBuffer[cellOffset].g = 0;
+      this->velocityBuffer[cellOffset].g *= -1;
+    } else if (this->colorBuffer[cellOffset].g > 255) {
+      this->colorBuffer[cellOffset].g = 255;
+      this->velocityBuffer[cellOffset].g *= -1;
+    }
+
+    if (this->colorBuffer[cellOffset].b < 0) {
+      this->colorBuffer[cellOffset].b = 0;
+      this->velocityBuffer[cellOffset].b *= -1;
+    } else if (this->colorBuffer[cellOffset].b > 255) {
+      this->colorBuffer[cellOffset].b = 255;
+      this->velocityBuffer[cellOffset].b *= -1;
+    }
+  }
+
+  void debugPixelValues(int offset) {
+    printf("Printing for pixel #%.2d\n", offset);
+    pixel p = this->colorPrimary[offset];
+    printf("Color primary %.2f %.2f %.2f\n", p.r, p.g, p.b);
+    p = this->colorBuffer[offset];
+    printf("Color buffer %.2f %.2f %.2f\n", p.r, p.g, p.b);
+    p = this->velocityPrimary[offset];
+    printf("Velocity primary %.2f %.2f %.2f\n", p.r, p.g, p.b);
+    p = this->velocityBuffer[offset];
+    printf("Velocity bufffer %.2f %.2f %.2f\n", p.r, p.g, p.b);
+  }
+
+  void debugPixel(char *name, pixel p) {
+    printf("%s %.2f %.2f %.2f\n", name, p.r, p.g, p.b);
   }
 
 public:
@@ -74,12 +170,71 @@ public:
     // seed the random number generator as it drives initial state
     std::srand(std::time(0));
     this->colorPrimary = this->initBufferToRandomColors();
-    // this->colorBuffer = this->copyBufferValues(this->colorPrimary);
+    this->colorBuffer = this->initBuffer();
+    this->copyBufferValues(this->colorPrimary, this->colorBuffer);
+    this->velocityPrimary = this->initBufferToZero();
+    this->velocityBuffer = this->initBufferToZero();
+    initNeighbors();
+  }
+
+  void tick() {
+    double minDistSquare = 64.0;
+    double sepNormMag = 4.0;
+    double ease = 0.67;
+
+    for (int y = 0; y < this->height; y++) {
+      for (int x = 0; x < this->width; x++) {
+        int cellOffset = getOffset(x, y);
+
+        // compute these values based on the values of the neighbours
+        pixel colorAverage = {.r = 0, .g = 0, .b = 0};
+        pixel velocityAverage = {.r = 0, .g = 0, .b = 0};
+        pixel sep = {.r = 0, .g = 0, .b = 0};
+
+        for (auto neighbor : this->neighbors[cellOffset]) {
+          int neighborOffset = getOffset(neighbor.x, neighbor.y);
+          add(&this->colorPrimary[neighborOffset], &colorAverage, &colorAverage);
+          add(&this->velocityPrimary[neighborOffset], &velocityAverage, &velocityAverage);
+
+          pixel d = {.r = 0, .g = 0, .b = 0};
+          subtract(&this->colorPrimary[cellOffset], &this->colorPrimary[neighborOffset], &d);
+
+          if (sumOfSquares(&d) < minDistSquare) {
+            add(&d, &sep, &sep);
+          }
+        }
+
+        double f = 1.0 / this->neighbors[cellOffset].size();
+        scale(&colorAverage, f, &colorAverage);
+        scale(&velocityAverage, f, &velocityAverage);
+
+        // normalize separation vector
+        if ((abs(sep.r) > 0.00001) || (abs(sep.g) > 0.00001) || (abs(sep.b) > 0.00001)) {
+
+          double length = sqrt(sumOfSquares(&sep));
+          scale(&sep, sepNormMag / length, &sep);
+        }
+
+        // update velocity by combining separation, alignment and cohesion effects
+        // change velocity only by 'ease' ratio
+        this->velocityBuffer[cellOffset].r += ease * (sep.r + velocityAverage.r + colorAverage.r - this->colorPrimary[cellOffset].r - this->velocityBuffer[cellOffset].r);
+        this->velocityBuffer[cellOffset].g += ease * (sep.g + velocityAverage.g + colorAverage.g - this->colorPrimary[cellOffset].g - this->velocityBuffer[cellOffset].g);
+        this->velocityBuffer[cellOffset].b += ease * (sep.b + velocityAverage.b + colorAverage.b - this->colorPrimary[cellOffset].b - this->velocityBuffer[cellOffset].b);
+
+        // update colors according to color velocities
+        add(&this->velocityBuffer[cellOffset], &this->colorBuffer[cellOffset], &this->colorBuffer[cellOffset]);
+        fixBoundaries(cellOffset);
+      }
+    }
+    this->copyBufferValues(this->velocityBuffer, this->velocityPrimary);
+    this->copyBufferValues(this->colorBuffer, this->colorPrimary);
+
+    this->currentTileY = 0;
   }
 
   emscripten::val nextTile() {
     if (this->currentTileY * TILE_SIZE > this->height) {
-      this->currentTileY = 0;
+      return emscripten::val::undefined();
     }
 
     // Generate a TILE_SIZE x TILE_SIZE array of pixels
@@ -90,8 +245,8 @@ public:
     for (int y = starty; y < endy; y++) {
       for (int x = startx; x < endx; x++) {
         // copy the color values from color primary into the buffer that gets returned to JS
-        size_t bufferOffset = ((x - startx) + (y - starty) * TILE_SIZE) * 4;
-        size_t addr = ((x - startx) + (y - starty) * TILE_SIZE);
+        int bufferOffset = ((x - startx) + (y - starty) * TILE_SIZE) * 4;
+        int addr = ((x - startx) + (y - starty) * TILE_SIZE);
         this->buffer[bufferOffset + 0] = this->colorPrimary[addr].r;
         this->buffer[bufferOffset + 1] = this->colorPrimary[addr].g;
         this->buffer[bufferOffset + 2] = this->colorPrimary[addr].b;
@@ -116,10 +271,30 @@ public:
 
     return returnVal;
   }
+
+  emscripten::val fullImage() {
+    // The image format that imageData expects is four unsigned bytes: red, green, blue, alpha
+    int bufferSize = this->width * this->height * 4;
+
+    for (int y = 0; y < this->height; y++) {
+      for (int x = 0; x < this->width; x++) {
+        // copy the color values from color primary into the buffer that gets returned to JS
+        int addr = x + y * this->width;
+        this->buffer[addr * 4 + 0] = this->colorPrimary[addr].r;
+        this->buffer[addr * 4 + 1] = this->colorPrimary[addr].g;
+        this->buffer[addr * 4 + 2] = this->colorPrimary[addr].b;
+        this->buffer[addr * 4 + 3] = 255;
+      }
+    }
+
+    return emscripten::val(emscripten::typed_memory_view(bufferSize, this->buffer));
+  }
 };
 
 EMSCRIPTEN_BINDINGS(hello) {
   emscripten::class_<Tapestry>("Tapestry")
       .constructor<int, int>()
-      .function("nextTile", &Tapestry::nextTile);
+      .function("nextTile", &Tapestry::nextTile)
+      .function("fullImage", &Tapestry::fullImage)
+      .function("tick", &Tapestry::tick);
 }
