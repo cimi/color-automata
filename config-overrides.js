fb0543eaeb29c8267aae31322b2d00f94dff1973
Alex Ciminian <alex.ciminian@gmail.com>
2017-08-05 13:43:52 +0100

    Remove wasm loader, import generated js module

--- config-overrides.js
+++ config-overrides.js
@@ -4,22 +4,6 @@ const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = function override(config, env) {
-  // see: https://github.com/ballercat/wasm-loader/issues/3 for more details
-  const wasmExtensionRegExp = /\.wasm$/;
-
-  config.resolve.extensions.push('.wasm');
-  // make the file loader ignore wasm files
-  const fileLoader = config.module.rules
-    .find(rule => rule.loader && rule.loader.endsWith(`${path.sep}file-loader${path.sep}index.js`));
-  fileLoader.exclude.push(wasmExtensionRegExp);
-
-  // and add a dedicated loader for them
-  config.module.rules.push({
-    test: wasmExtensionRegExp,
-    include: path.resolve(__dirname, "src"),
-    use: [{loader: require.resolve('wasm-loader'), options: {}}]
-  });
-
   const copyWebpackPlugin = new CopyWebpackPlugin([{
     from: 'src/wasm/*.wasm',
     to: 'wasm/[name].wasm'
