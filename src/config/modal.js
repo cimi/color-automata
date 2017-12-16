45139acb8781a2e4377b35600c239f1e8e20af32
Alex Ciminian <alex.ciminian@gmail.com>
2017-12-16 16:30:55 +0000

    Extract functions for creating elements in the config modal

--- src/config/modal.js
+++ src/config/modal.js
@@ -17,39 +17,39 @@ export const startTapestry = () => {
   updateConfig({ currentIntervalId: intervalId });
 };
 
+const input = (config, name, type = "number") =>
+  `<input name="${name}" type="${type}" value="${config[name]}"></input>`;
+
+const radio = (config, name, value) =>
+  `<input name="${name}" type="radio" value="${value}" ${
+    config[name] === value ? "checked" : ""
+  } />`;
+
+const checkbox = (config, name) =>
+  `<input type="checkbox" name="${name}" ${config[name] ? "checked" : ""} />`;
+
 // TODO: configure cell size instead of choosing width and height
 // that way there's no distortion and you can play better with granularity
 export const openConfigurationModal = () => {
-  const tapestryConfiguration = getConfig();
+  const config = getConfig();
   vex.dialog.buttons.YES.text = "Reset";
   // TODO: currently running configuration instead of description
   vex.dialog.open({
-    message: `You are looking at a combination between a cellular automaton and a flocking algorithm that yields a color tapestry.
-      Use the controls below to reset the tapestry to a different configuration.`,
+    message: `This visualisation emerges from combining a cellular automaton
+    with a flocking algorithm. Tweak the settings below for different output.`,
     input: `<div id="configuration-form">
         <div class="grid-size">
           <span>Cell size</span>
-          <input name="height" type="number" value="${
-            tapestryConfiguration.height
-          }"></input>
-          <input name="width" type="number" value="${
-            tapestryConfiguration.width
-          }"></input>
+          ${input(config, "height")} ${input("width")}
         </div>
         <div class="implementation">
           <span>Implementation</span>
-          <label><input type="radio" name="implementation" value="js" ${
-            tapestryConfiguration.implementation === "js" ? "checked" : ""
-          } />JS</label>
-          <label><input type="radio" name="implementation" value="wasm" ${
-            tapestryConfiguration.implementation === "wasm" ? "checked" : ""
-          } />WASM</label>
+          <label>${radio(config, "implementation", "js")} JS</label>
+          <label>${radio(config, "implementation", "wasm")} WASM</label>
         </div>
         <div class="show-fps">
           <span></span>
-          <label for="debug"><input type="checkbox" name="debug" ${
-            tapestryConfiguration.debug ? "checked" : ""
-          } />Console debugging</label>
+          <label for="debug">${checkbox(config, "debug")} Debug mode</label>
         </div>
       <p class="credits"> Inspired by <a href="http://rectangleworld.com/blog/archives/587">Rectangleworld</a>.
       More details on <a href="https://github.com/cimi/color-automata">on GitHub</a>!</p>
