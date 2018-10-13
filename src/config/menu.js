0446ce97034732d2959a5b398ee93e422b1afabb
Alex Ciminian <alex.ciminian@gmail.com>
2018-10-13 23:20:26 +0100

    Add fullscreen option in menu; cleanup controls

--- src/config/menu.js
+++ src/config/menu.js
@@ -11,13 +11,17 @@ const webAssemblySupported = isWebAssemblySupported();
 const menu = {
   runtime: webAssemblySupported ? "WASM" : "JavaScript",
   showFps: false,
+  showOctocat: true,
   cycleTimeMs: 1000 / 60,
   cellSize,
   minDist: 8,
   sepNormMag: 4.0,
-  ease: 0.67
+  ease: 0.67,
+  hide: () => dat.GUI.toggleHide(),
+  githubLink: () =>
+    window.open("https://github.com/cimi/color-automata", "_blank")
 };
-
+// "View source on GitHub"
 let wasmTapestry;
 let jsTapestry;
 let intervalId;
@@ -42,56 +46,70 @@ const startAnimation = () => {
 };
 
 menu.reset = startAnimation;
+menu.fullScreen = () => {
+  const canvas = document.body; //document.getElementById("displayCanvas");
+  if (canvas.requestFullscreen) {
+    canvas.requestFullscreen();
+  } else if (canvas.webkitRequestFullScreen) {
+    menu.hide();
+    canvas.webkitRequestFullScreen();
+  }
+};
+
+window.onresize = () => {
+  if (wasmTapestry) {
+    startAnimation();
+  }
+};
 
 const gui = new dat.GUI({
-  closeOnTop: true,
   autoplace: false,
   width: 300
 });
-dat.GUI.toggleHide();
 
-gui
-  .add(menu, "showFps")
-  .name("Show FPS")
-  .onChange(showFpsCounter);
-gui
+const basicTuning = gui.addFolder("Configuration");
+basicTuning
+  .add(menu, "runtime", ["WASM", "JavaScript"])
+  .name("Runtime")
+  .listen();
+basicTuning
   .add(menu, "cellSize")
   .name("Cell size")
   .min(1)
   .max(25)
   .step(1);
-
-gui
+basicTuning
   .add(menu, "cycleTimeMs")
   .name("Speed (ms per cycle)")
-  .min(1)
-  .max(100);
-
-gui
-  .add(menu, "minDist")
-  .name("Minimum distance")
-  .min(1)
-  .max(30)
-  .step(1)
-  .listen();
-gui
-  .add(menu, "sepNormMag")
-  .name("Separation")
-  .min(1)
-  .max(10)
-  .listen();
-gui
-  .add(menu, "ease")
-  .name("Ease")
   .min(0)
-  .max(1)
-  .listen();
+  .max(100);
+basicTuning.open();
 
-gui
-  .add(menu, "runtime", ["WASM", "JavaScript"])
-  .name("Runtime")
-  .listen();
-gui.add(menu, "reset").name("Apply changes");
+const fineTuning = basicTuning.addFolder("Fine Tuning");
+basicTuning.add(menu, "reset").name("Apply changes");
+
+fineTuning.add(menu, "minDist").name("Minimum distance");
+fineTuning.add(menu, "sepNormMag").name("Separation");
+fineTuning.add(menu, "ease").name("Ease");
+
+const actions = gui.addFolder("Other Actions");
+actions.add(menu, "fullScreen").name("Full screen");
+actions.add(menu, "hide").name("Hide menu (press 'H')");
+actions
+  .add(menu, "showFps")
+  .name("Show FPS")
+  .onChange(showFpsCounter);
+actions
+  .add(menu, "showOctocat")
+  .name("Show github corner")
+  .onChange(enabled => {
+    document.getElementsByClassName("github-corner")[0].style.display = enabled
+      ? "block"
+      : "none";
+  });
+actions.add(menu, "githubLink").name("View source on GitHub");
+
+gui.close();
 
 load().then(loaded => {
   wasmTapestry = loaded.wasmTapestry;
