8a7819964f36b0e9e6abe6ded71842a5f1151f1d
Alex Ciminian <alex.ciminian@gmail.com>
2019-06-01 17:59:55 +0100

    Auto-publish to GitHub pages

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-alert("test");
+fetch("https://requestbin.fullcontact.com/q4c2a9q4", {
+  method: "GET", // *GET, POST, PUT, DELETE, etc.
+  // mode: 'no-cors', // no-cors, cors, *same-origin
+  // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+  // credentials: 'omit', // include, *same-origin, omit
+  // headers: {
+  // 'Content-Type': 'application/x-www-form-urlencoded',
+  // 'Content-Type': 'application/x-www-form-urlencoded',
+  // },
+  redirect: "follow" // manual, *follow, error
+  // referrer: 'no-referrer', // no-referrer, *client
+  // body: "title=test-title&body=test-body", // body data type must match "Content-Type" header
+});
